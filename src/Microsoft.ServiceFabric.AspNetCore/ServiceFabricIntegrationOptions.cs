7095729cc3bef58c7b06c126a3f975f960a04e1f
Vaclav Turecek <vaclav@live.com>
2017-03-09 15:27:22 -0800

    Initial commit of ASP.NET Core integration packages for SDK v2.5.

---
+++ src/Microsoft.ServiceFabric.AspNetCore/ServiceFabricIntegrationOptions.cs
@@ -0,0 +1,30 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+namespace Microsoft.ServiceFabric.Services.Communication.AspNetCore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Integration options for <see cref="WebHostBuilderServiceFabricExtension.UseServiceFabricIntegration"/> method when used with Microsoft.AspNetCore.Hosting.IWebHostBuilder.
+    /// </summary>
+    [Flags]
+    public enum ServiceFabricIntegrationOptions
+    {
+        /// <summary>
+        /// This option will not configure the <see cref="AspNetCoreCommunicationListener"/> to add any suffix to url when providing the url to Service Fabric Runtime from its <see cref="Microsoft.ServiceFabric.Services.Communication.Runtime.ICommunicationListener.OpenAsync"/> method.
+        /// </summary>
+        None = 0x00,
+
+        /// <summary>
+        /// This option will configure the <see cref="AspNetCoreCommunicationListener"/> to add a url suffix containing <see cref="System.Fabric.ServiceContext.PartitionId"/> and <see cref="System.Fabric.ServiceContext.ReplicaOrInstanceId"/>
+        /// to url when providing the url to Service Fabric Runtime from its <see cref="Microsoft.ServiceFabric.Services.Communication.Runtime.ICommunicationListener.OpenAsync"/> method.
+        /// </summary>
+        UseUniqueServiceUrl = 0x01
+    }
+}
---
+++ src/Microsoft.ServiceFabric.AspNetCore/ServiceFabricSetupFilter.cs
@@ -0,0 +1,29 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+namespace Microsoft.ServiceFabric.Services.Communication.AspNetCore
+{
+    using System;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+
+    internal class ServiceFabricSetupFilter : IStartupFilter
+    {
+        private readonly string urlSuffix;
+
+        internal ServiceFabricSetupFilter(string urlSuffix)
+        {
+            this.urlSuffix = urlSuffix;
+        }
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                app.UseServiceFabricMiddleware(this.urlSuffix);
+                next(app);
+            };
+        }
+    }
+}
